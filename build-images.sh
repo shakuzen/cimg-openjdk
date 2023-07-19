d964563d94d1f2c9a58f0675a264cc4c4a25ca7e
Adam Hartley <git@ahartley.com>
2023-07-19 17:01:28 +0100

    Test build

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+set -eo pipefail
 
-docker build --file 20.0/Dockerfile -t cimg/openjdk:20.0.1 -t cimg/openjdk:20.0 --platform linux/amd64 .
-docker build --file 20.0/node/Dockerfile -t cimg/openjdk:20.0.1-node -t cimg/openjdk:20.0-node --platform linux/amd64 .
-docker build --file 20.0/browsers/Dockerfile -t cimg/openjdk:20.0.1-browsers -t cimg/openjdk:20.0-browsers --platform linux/amd64 .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 20.0/Dockerfile -t cimg/openjdk:20.0.1 -t cimg/openjdk:20.0 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 20.0/node/Dockerfile -t cimg/openjdk:20.0.1-node -t cimg/openjdk:20.0-node --push .
+docker buildx build --platform=linux/amd64 --file 20.0/browsers/Dockerfile -t cimg/openjdk:20.0.1-browsers -t cimg/openjdk:20.0-browsers --push .
